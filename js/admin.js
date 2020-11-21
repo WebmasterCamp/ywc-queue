8ba807c9cc6c60c7c9cc3d7ec0cb99db4c53911d
Phumrapee Limpianchop <git@rayriffy.com>
2020-11-21 11:31:31 +0900

    chore: password on init

--- js/admin.js
+++ js/admin.js
@@ -1,68 +1,80 @@
-const branch = window.location.search.split('branch=')[1]
+const branch = window.location.search.split("branch=")[1];
 
 const setup = () => {
   if (!(branch && getPrefix(branch))) {
-    window.location = `./index.html`
+    window.location = `./index.html`;
   }
 
   firebase.auth().onAuthStateChanged((user) => {
     if (!user) {
-      window.location = './index.html'
+      try {
+        const password = prompt("Enter password");
+        firebase
+          .auth()
+          .signInWithEmailAndPassword("ywc@webmaster.or.th", password)
+          .then(() => alert("Welcome Admin!"))
+          .catch(() => {
+            window.location = "./index.html";
+          });
+      } catch {
+        window.location = "./index.html";
+      }
+      // window.location = './index.html'
     }
-  })
+  });
 
-  document.getElementById('branch').innerText = `Web ${capitalize(branch)}`
-}
+  document.getElementById("branch").innerText = `Web ${capitalize(branch)}`;
+};
 
 const capitalize = (text) =>
-  text.substring(0, 1).toUpperCase() + text.substring(1)
+  text.substring(0, 1).toUpperCase() + text.substring(1);
 
 const modifyQueue = (amount) => {
   const queue =
-    parseInt(document.getElementById('current-queue').value) + amount
-  document.getElementById('current-queue').value = queue
+    parseInt(document.getElementById("current-queue").value) + amount;
+  document.getElementById("current-queue").value = queue
     .toString()
-    .padStart(2, '0')
-}
+    .padStart(2, "0");
+};
 
 const publishQueue = () => {
   firebase
     .database()
     .ref(`ywc-queue/${branch}`)
     .set({
-      current: document.getElementById('current-queue').value,
-      custom: '',
+      current: document.getElementById("current-queue").value,
+      custom: "",
     })
-    .catch((err) => alert(err))
-}
+    .catch((err) => alert(err));
+};
 
 const publishCustomText = () => {
   firebase
     .database()
     .ref(`ywc-queue/${branch}`)
     .update({
-      custom: document.getElementById('custom-queue').value,
+      custom: document.getElementById("custom-queue").value,
     })
-    .catch((err) => alert(err))
-}
+    .catch((err) => alert(err));
+};
 
-setup()
+setup();
 
 firebase
   .database()
   .ref(`ywc-queue/${branch}`)
-  .once('value')
+  .once("value")
   .then((snapshot) => {
     document.getElementById(
-      'current-queue'
-    ).value = snapshot.val().current.padStart(2, '0')
-  })
+      "current-queue"
+    ).value = snapshot.val().current.padStart(2, "0");
+  });
 
 firebase
   .database()
   .ref(`ywc-queue/${branch}`)
-  .on('value', (snapshot) => {
-    const value = snapshot.val()
-    document.getElementById('on-screen').innerText =
-      value.custom !== '' ? value.custom : getPrefix(branch) + value.current
-  })
+  .on("value", (snapshot) => {
+    const value = snapshot.val();
+    document.getElementById("on-screen").innerText =
+      value.custom !== "" ? value.custom : getPrefix(branch) + value.current;
+  });
